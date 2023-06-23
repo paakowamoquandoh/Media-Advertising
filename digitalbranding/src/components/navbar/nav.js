22ebe42f545dc8c2d82a6f68e28a4f6f6146f16a
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-06-23 16:55:42 +0000

    icons added

--- digitalbranding/src/components/navbar/nav.js
+++ digitalbranding/src/components/navbar/nav.js
@@ -5,7 +5,13 @@ class NavBar extends Component{
     render(){
         return(
             <nav className="navItems">
-                
+                <h1 className="navLogo">Media<i>Rebrand</i></h1>
+
+                <ul className="navMenu">
+                    <li>
+                        <link>Home</link>
+                    </li>
+                </ul>
             </nav>
         )
     }
