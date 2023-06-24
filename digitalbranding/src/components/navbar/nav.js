36f9b5e90a66e0ef5177b0711bdaadd0634c25c8
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-06-24 14:49:34 +0000

    nav commit

--- digitalbranding/src/components/navbar/nav.js
+++ digitalbranding/src/components/navbar/nav.js
@@ -1,48 +1,47 @@
-import React, { Component, useState } from 'react';
+import { Component } from "react";
 import { Menu, XCircle } from 'react-feather';
 import { menuList } from "./navLinks";
 import "./nav.css";
 
-class NavBar extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-          isMenuOpen: false,
-        };
-      }
-    
-      toggleMenu = () => {
-        this.setState(prevState => ({
-          isMenuOpen: !prevState.isMenuOpen,
-        }));
-      };
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMenuOpen: false,
+    };
+  }
 
-    render(){
-        const { isMenuOpen } = this.state;
+  toggleMenu = () => {
+    this.setState((prevState) => ({
+      isMenuOpen: !prevState.isMenuOpen,
+    }));
+  };
 
-        return(
-            <nav className="navItems">
-                <h1 className="navLogo">Media<i className="reColor">Rebrand</i></h1>
+  render() {
+    const { isMenuOpen } = this.state;
 
-                <div className="menuIcons" onClick={this.toggleMenu}>
-                    {isMenuOpen ? <XCircle /> : <Menu />}
-                </div>
+    return (
+      <nav className="navItems">
+        <h1 className="navLogo">
+          Media<i className="reColor">Rebrand</i>
+        </h1>
 
-                <ul className="navMenu">
-                    {menuList.map((item,index) => {
-                        return(
-                            <li key={index} className={item.cName}>
-                                <a href={item.url}>
-                                    {item.title}
-                                </a>
-                            </li>
-                        )
-                    })}
-                </ul>
-            </nav>
-        )
-    }
+        <div className="menuIcon" onClick={this.toggleMenu}>
+          {isMenuOpen ? <XCircle /> : <Menu />}
+        </div>
+
+        <ul className={`navMenu ${isMenuOpen ? 'active' : ''}`}>
+          {menuList.map((item, index) => {
+            return (
+              <li key={index} className={item.cName}>
+                <a href={item.url}>{item.title}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    );
+  }
 }
-
 
 export default NavBar;
