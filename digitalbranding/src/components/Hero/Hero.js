7e4a38ff95fe287828335575158699972d94a9b0
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-06-24 18:18:23 +0000

    images

---
+++ digitalbranding/src/components/Hero/Hero.js
@@ -0,0 +1,13 @@
+import {  } from "./hero.css";
+
+function HeroSection() {
+    return(
+        <>
+        <div className="heroSection">
+           <img alt="heroImage" src=""/>
+        </div>
+        </>
+    )    
+}
+
+export default HeroSection;
