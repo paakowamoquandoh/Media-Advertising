46f63928ae8d9b55dd12fe286a94518f4c0bcfcf
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-06-24 13:38:45 +0000

    nav commit

--- digitalbranding/src/components/navbar/nav.js
+++ digitalbranding/src/components/navbar/nav.js
@@ -1,16 +1,31 @@
-import { Component } from "react";
-import { Menu } from 'react-feather';
+import React, { Component, useState } from 'react';
+import { Menu, XCircle } from 'react-feather';
 import { menuList } from "./navLinks";
 import "./nav.css";
 
 class NavBar extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          isMenuOpen: false,
+        };
+      }
+    
+      toggleMenu = () => {
+        this.setState(prevState => ({
+          isMenuOpen: !prevState.isMenuOpen,
+        }));
+      };
+
     render(){
+        const { isMenuOpen } = this.state;
+
         return(
             <nav className="navItems">
                 <h1 className="navLogo">Media<i className="reColor">Rebrand</i></h1>
 
-                <div className="menuIcon">
-                    <Menu />
+                <div className="menuIcons" onClick={this.toggleMenu}>
+                    {isMenuOpen ? <XCircle /> : <Menu />}
                 </div>
 
                 <ul className="navMenu">
