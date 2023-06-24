9ff92fb41923037ec3ea76f2a6b9113af9423c7d
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-06-24 18:45:34 +0000

    home hero added

--- digitalbranding/src/components/Hero/Hero.js
+++ digitalbranding/src/components/Hero/Hero.js
@@ -1,13 +1,30 @@
 import {  } from "./hero.css";
 
-function HeroSection() {
+function HeroSection(props) {
     return(
         <>
-        <div className="heroSection">
-           <img alt="heroImage" src=""/>
+        <div className={props.className}>
+           <img alt="heroImage" src={props.heroImage}/>
+
+           <div className="heroText">
+            <h1>{props.title}</h1>
+            <p>{props.text }</p>
+            <a href="/">
+            Request a Quote
+            </a>
+           </div>
         </div>
         </>
     )    
 }
+
+
+{/* <div className="heroText">
+            <h1></h1>
+            <p></p>
+            <a href="/">
+            Request a Quote
+            </a>
+           </div> */}
 
 export default HeroSection;
--- digitalbranding/src/routes/Home.js
+++ digitalbranding/src/routes/Home.js
@@ -1,10 +1,17 @@
 import NavBar from "../components/navbar/nav";
+import Hero from "../components/Hero/Hero";
 
 
 function HomePage() {
     return(
         <>
         <NavBar/>
+        <Hero 
+        className="homeHero"
+        heroImage="https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+        title="Transform your Brand"
+        text="Choose our revolutionary Digital experiences."
+        />
         </>
     )    
 }
