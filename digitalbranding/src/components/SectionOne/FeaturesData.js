a09c93e2c8911ca8109723da79b75b81ad545d7f
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-06-26 09:50:58 +0000

    more features

--- digitalbranding/src/components/SectionOne/FeaturesData.js
+++ digitalbranding/src/components/SectionOne/FeaturesData.js
@@ -9,21 +9,19 @@ import featImg4 from "../../media/images/media.webp";
 class FeaturesData extends Component{
     render(){
         return(
-            
-            <div className="firstFeature">
+            <div className={this.props.className}>
                 <div className="featText">
-                    <h2>Create A Unique Brand Identity</h2>
-                    <p>Crafting a unique brand identity is an essential undertaking for any business or organization. It goes beyond merely having a recognizable logo or catchy tagline; it encompasses the core values, personality, and promise that set a brand apart from its competitors.
-
-                       To create a unique brand identity, one must delve into understanding the target audience and their desires, needs, and aspirations. This knowledge serves as the foundation upon which the brand's identity is built. By aligning the brand's values with those of the target audience, a deep emotional connection can be forged, fostering loyalty and advocacy.
-
-                       Consistency is key</p>
+                    <h2>{this.props.heading}</h2>
+                    <p>{this.props.description}</p><hr/>
+                    <a href="" className="featBtn">{this.props.button}</a>
                 </div>
                 <div className="featImage">
-                     <img alt="featureimg" src={featImg1} />
-                     <img alt="featureimg" src={featImg2} />
+                     <img alt="featureimg" src={this.props.image1} />
+                     <img alt="featureimg" src={this.props.image2} />
                 </div>
             </div>
+            
+           
         )
     }
 }
--- digitalbranding/src/components/SectionOne/Section.js
+++ digitalbranding/src/components/SectionOne/Section.js
@@ -10,7 +10,28 @@ const FeaturesSection = () => {
         <div className="mainSection">
             <h1>Our Main Features</h1>
             <p>Integrating brand strategies with digital initiatives to improve consumer experiences across touchpoints</p>
-            <FeaturesData/>
+           
+            <FeaturesData
+            className="firstFeature"
+            heading="Create A Unique Brand Identity"
+            description="Crafting a unique brand identity is an essential undertaking for any business or organization. It goes beyond merely having a recognizable logo or catchy tagline; it encompasses the core values, personality, and promise that set a brand apart from its competitors.
+
+            To create a unique brand identity, one must delve into understanding the target audience and their desires, needs, and aspirations. This knowledge serves as the foundation upon which the brand's identity is built. By aligning the brand's values with those of the target audience, a deep emotional connection can be forged, fostering loyalty and advocacy.
+
+            Consistency is key"
+            button="Request a Proposal"
+            image1={featImg1}
+            image2={featImg2}
+            />
+
+            <FeaturesData
+            className="secondFeature"
+            heading="Engage With Your Audience"
+            description="Experience the essence of effective communication and connection in various contexts, whether it be business, education, or entertainment. When we engage with our audience, we actively involve and interact with them, fostering a deeper connection and creating a more meaningful experience"
+            button="Learn More"
+            image1={featImg4}
+            image2={featImg3}
+            />
         </div>
     )
 }
