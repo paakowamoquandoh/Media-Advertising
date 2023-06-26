45892fbab198405134eece73146a0b6ce77408cb
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-06-26 14:27:31 +0000

    footer starts

---
+++ digitalbranding/src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+import "./footer.css";
+
+const Footer = () => {
+    return(
+        <div className="footer">
+            <div className="topside">
+                <div>
+                <h1>Media<i className="reColor">Rebrand</i></h1>
+                <p>Results-Driven Digital Marketing</p>
+                </div>
+            </div>
+        </div>
+    )
+}
